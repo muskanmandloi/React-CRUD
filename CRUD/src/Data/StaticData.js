193a400598b404b82c223bfee66141c9f57233d8
MuskanMandloi <muskanmandloi15112000@gmail.com>
2023-03-13 18:23:44 +0530

    first commit

---
+++ CRUD/src/Data/StaticData.js
@@ -0,0 +1,60 @@
+const EmployeesData = [
+    {
+        id: 1,
+        firstName: 'Muskan',
+        lastName: 'Mandloi',
+        email: 'mandloi@gmail.com',
+        salary: '100000',
+        date: '2023-06-01'
+    },
+    {
+        id: 2,
+        firstName: 'Mehak',
+        lastName: 'Patidar',
+        email: 'mehak@gmail.com',
+        salary: '50000',
+        date: '2020-05-11'
+    },
+    {
+        id: 3,
+        firstName: 'Ravi',
+        lastName: 'Gupta',
+        email: 'gupta@gmail.com',
+        salary: '90000',
+        date: '2021-11-11'
+    },
+    {
+        id: 4,
+        firstName: 'Gopi',
+        lastName: 'Kisan',
+        email: 'gopi@gmail.com',
+        salary: '70000',
+        date: '2020-01-01'
+    },
+    {
+        id: 5,
+        firstName: 'Lokesh',
+        lastName: 'Mandloi',
+        email: 'lokesh@gmail.com',
+        salary: '400000',
+        date: '2021-09-15'
+    },
+    {
+        id: 6,
+        firstName: 'Vijay',
+        lastName: 'Das',
+        email: 'vijay@gmail.com',
+        salary: '420000',
+        date: '2021-09-10'
+    },
+    {
+        id: 7,
+        firstName: 'Sunil',
+        lastName: 'Grover',
+        email: 'sunil@gmail.com',
+        salary: '450000',
+        date: '2020-02-01'
+    },
+]
+
+export { EmployeesData };
